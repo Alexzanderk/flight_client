a7c816dc80a67f6db568aaa451896bd19811cb9e
Alexzanderk <kotsarev.a@gmail.com>
2019-05-13 15:29:06 +0300

    Add graphQL schema

---
+++ src/graphql/queries.js
@@ -0,0 +1,44 @@
+import gql from 'graphql-tag';
+export const ME_QUERY = `
+{
+  me {
+    _id
+    name,
+    email
+    picture
+  }
+}
+`;
+
+export const SEARCH_CITY_QUERY = `
+query SEARCH_CITY($query: String! $limit: Int){
+  searchCity(query: $query limit: $limit) {
+    _id
+    name
+    flightable
+    name_translations{
+      ru
+      en
+    }
+    code
+    airports {
+      _id
+      name
+      flightable
+      name_translations{
+        ru
+        en
+      }
+      code
+      city_code
+      city {
+        name
+        name_translations{
+          ru
+          en
+        }
+      }
+    }
+  }
+}
+`;
